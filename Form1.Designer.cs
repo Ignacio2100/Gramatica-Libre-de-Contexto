5c85b53dacaa86d54ac0375e046ae8ac2f065f82
CENTRAL <CENTRAL@CENTRAL>
2022-10-12 15:44:32 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace ProduccionesGramatica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCabezas = new System.Windows.Forms.TextBox();
+            this.txtCuerpos = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Prods = new System.Windows.Forms.DataGridView();
+            this.btnDerivar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Prods)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(294, 38);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Cuerpo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(57, 38);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cabeza";
+            // 
+            // txtCabezas
+            // 
+            this.txtCabezas.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCabezas.Location = new System.Drawing.Point(15, 65);
+            this.txtCabezas.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCabezas.Name = "txtCabezas";
+            this.txtCabezas.Size = new System.Drawing.Size(174, 25);
+            this.txtCabezas.TabIndex = 0;
+            // 
+            // txtCuerpos
+            // 
+            this.txtCuerpos.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCuerpos.Location = new System.Drawing.Point(256, 65);
+            this.txtCuerpos.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCuerpos.Name = "txtCuerpos";
+            this.txtCuerpos.Size = new System.Drawing.Size(174, 25);
+            this.txtCuerpos.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnAgregar.FlatAppearance.BorderSize = 2;
+            this.btnAgregar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.btnAgregar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnAgregar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.btnAgregar.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(447, 62);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(91, 29);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 62);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "---->";
+            // 
+            // Prods
+            // 
+            this.Prods.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Prods.Location = new System.Drawing.Point(16, 35);
+            this.Prods.Margin = new System.Windows.Forms.Padding(2);
+            this.Prods.Name = "Prods";
+            this.Prods.RowHeadersWidth = 51;
+            this.Prods.RowTemplate.Height = 24;
+            this.Prods.Size = new System.Drawing.Size(254, 233);
+            this.Prods.TabIndex = 1;
+            // 
+            // btnDerivar
+            // 
+            this.btnDerivar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnDerivar.FlatAppearance.BorderSize = 2;
+            this.btnDerivar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.btnDerivar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnDerivar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.btnDerivar.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDerivar.Location = new System.Drawing.Point(170, 20);
+            this.btnDerivar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDerivar.Name = "btnDerivar";
+            this.btnDerivar.Size = new System.Drawing.Size(99, 29);
+            this.btnDerivar.TabIndex = 0;
+            this.btnDerivar.Text = "Derivar";
+            this.btnDerivar.UseVisualStyleBackColor = true;
+            this.btnDerivar.Click += new System.EventHandler(this.btnDerivar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.AliceBlue;
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.btnlimpiar);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtResultado);
+            this.groupBox1.Controls.Add(this.btnDerivar);
+            this.groupBox1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(11, 288);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(570, 129);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Respuesta: ";
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnlimpiar.FlatAppearance.BorderSize = 2;
+            this.btnlimpiar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.btnlimpiar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnlimpiar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.btnlimpiar.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(290, 20);
+            this.btnlimpiar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(99, 29);
+            this.btnlimpiar.TabIndex = 1;
+            this.btnlimpiar.Text = "Salir";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 67);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(311, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "La Cadena Derivada de la Gramática es:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.Color.White;
+            this.txtResultado.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.ForeColor = System.Drawing.Color.Red;
+            this.txtResultado.Location = new System.Drawing.Point(326, 67);
+            this.txtResultado.Margin = new System.Windows.Forms.Padding(2);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(228, 30);
+            this.txtResultado.TabIndex = 2;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.AliceBlue;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.btnAgregar);
+            this.groupBox2.Controls.Add(this.txtCuerpos);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtCabezas);
+            this.groupBox2.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 139);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(570, 127);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ingrese Producciones (Si una de sus produciones es Lambda solo de un Espacio): ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.AliceBlue;
+            this.groupBox3.Controls.Add(this.Prods);
+            this.groupBox3.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(603, 139);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(286, 278);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Datos Ingresados: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-4, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(908, 524);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(30, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(129, 124);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(38, 168);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 50);
+            this.panel1.TabIndex = 13;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox4.BackgroundImage")));
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.pictureBox2);
+            this.groupBox4.Location = new System.Drawing.Point(-4, -4);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(908, 142);
+            this.groupBox4.TabIndex = 14;
+            this.groupBox4.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(165, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(712, 67);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Gramática Libre De Contexto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(901, 518);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Automatas y Lenguajes Formales";
+            ((System.ComponentModel.ISupportInitialize)(this.Prods)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView Prods;
+        private System.Windows.Forms.Button btnDerivar;
+        private System.Windows.Forms.TextBox txtCuerpos;
+        private System.Windows.Forms.TextBox txtCabezas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProduccionesGramatica
+{
+    public partial class Form1 : Form
+    {
+        List<string> cabezas = new List<string>();
+        List<string> cabezasOtrasProducciones = new List<string>();
+        List<string> cuerpos = new List<string>();
+        List<Producciones> MostradoDatos = new List<Producciones>();//lista de objetos que el usuario va a percibir
+        List<string> cuerpoPrincipal = new List<string>();
+        //DataTable producciones = new DataTable();
+        string letrasIniciales = ""; //toma el cuerpo de la primera produccion.
+        string cadenaFinal = "";//almacena lo que es la derivacion final de la gramatica ingresada.
+        List<int> size = new List<int>();
+        int cantCabezas;
+        List<string> derivado = new List<string>();//son las derivaciones de cada cabeza
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+        public  void refrescarGridView()
+        {
+            this.Prods.DataSource = "";
+        }
+        public void refrescarTextbox()
+        {
+            this.txtCabezas.Clear();
+            this.txtCuerpos.Clear();
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            
+            Producciones objeto = new Producciones();
+            objeto.Cabeza = this.txtCabezas.Text;
+            objeto.Cuerpo = this.txtCuerpos.Text;
+            MostradoDatos.Add(objeto);
+            this.Prods.DataSource = null;
+            this.Prods.DataSource = MostradoDatos;
+            //for (int i = 0; i < cabezas.Count; i++)
+            //{
+            //    producciones.Rows.Add(cabezas[i], cuerpos[i]);
+            //}
+            cabezas.Add(this.txtCabezas.Text);
+            cuerpos.Add(this.txtCuerpos.Text);
+            refrescarTextbox();
+        }
+
+        private void btnDerivar_Click(object sender, EventArgs e)
+        {
+             letrasIniciales = cuerpos[0].ToString();//almacena el primer cuerpo que en teoria es el principal de la gramatica
+
+            for (int a = 0; a < letrasIniciales.Length; a++)
+            {
+                cuerpoPrincipal.Add(letrasIniciales[a].ToString());//se desgloza letra por letra del cuerpo princpal para despues hacer la derivacion en base a esas letras
+            }
+            cantCabezas = cabezas.Count - 1;//toas las cabezas menos la primera que es la principal.
+            //guardamos las cabezas y los cuerpos a operar:
+            for (int z = 1; z < cabezas.Count; z++)
+            {
+                cabezasOtrasProducciones.Add(cabezas[z]);
+                derivado.Add(cuerpos[z]);
+            }
+            //se ordena lo que son las cabezas para luego compararlas y asi ir sustituyendo las variables.
+            
+            for (int b = 0; b < cantCabezas; b++)
+            {
+                size.Add(derivado[b].Count());
+            }
+            int aux1;
+            string aux2, aux3;
+            for (int m = 1; m < size.Count; m++)
+            {
+                aux1 = size[m];
+                aux2 = cabezasOtrasProducciones[m];
+                aux3 = derivado[m];
+                for (int z = m - 1; z >= 0 && size[z] < aux1; z--)
+                {
+                    size[z + 1] = size[z];
+                    cabezasOtrasProducciones[z + 1] = cabezasOtrasProducciones[z];
+                    derivado[z + 1] = derivado[z];
+
+                    size[z] = aux1;
+                    cabezasOtrasProducciones[z] = aux2;
+                    derivado[z] = aux3;
+                }
+            }
+            //reemplazo de las derivaciones
+            for (int b = 0; b < cantCabezas; b++)
+            {
+                for (int t = 0; t < cuerpoPrincipal.Count; t++)
+                {
+                    cuerpoPrincipal[t] = cuerpoPrincipal[t].Replace(cabezasOtrasProducciones[b], derivado[b]);
+                }
+            }
+            //almacena el derivado de la gramatica 
+            for (int y = 0; y < cuerpoPrincipal.Count; y++)
+            {
+                cadenaFinal = cadenaFinal + cuerpoPrincipal[y];
+            }
+            this.txtResultado.Text = cadenaFinal.ToString();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            //this.txtCabezas.Text = "";
+            //this.txtCuerpos.Text = "";
+            //this.txtResultado.Text = "";
+            //this.Prods.Rows.Clear();
+            this.Close();
+        }
+
+        //private void btnLimpiar_Click(object sender, EventArgs e)
+        //{
+        //    refrescarGridView();
+        //    cabezas.Clear();
+        //    cuerpos.Clear();
+        //    cuerpoPrincipal.Clear();
+        //    derivado.Clear();
+        //    this.txtResultado.Text = "";
+        //    MostradoDatos.Clear();
+        //    this.Prods.DataSource = null;
+        //    size.Clear();
+        //    letrasIniciales = "";
+        //    cadenaFinal = "";
+        //}
+    }
+}
